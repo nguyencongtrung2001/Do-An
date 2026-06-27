8f1ca5228adfd741201044a17b6808322df1aff2
nguyen cong trung <congtrung1004@gmail.com>
2026-06-27 09:20:40 +0700

    Fix Leaflet Icon TypeScript error for Vercel build

--- frontend/src/components/map/VenueMapSelector.tsx
+++ frontend/src/components/map/VenueMapSelector.tsx
@@ -8,7 +8,7 @@ import { OpenStreetMapProvider } from "leaflet-geosearch";
 
 
 
-delete L.Icon.Default.prototype._getIconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
